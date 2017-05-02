bf40ddb5582f047bd414868a16a885e57f6a4085
unknown <bruninfante@gmail.com>
2017-05-02 19:55:05 +0100

    submit changes to html form

--- teste.php
+++ teste.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	
+
 	
 	
 ?>
@@ -22,97 +22,45 @@
 			<h2>Discount Calculator</h2>
 		  </header>
 		  
-		  <div>
-			<label class="desc" id="title1" for="Field1">Client</label>
-			<div>
-			  <input id="Field1" name="Field1" type="text" class="field text fn" value="" size="8" tabindex="1">
+		  <div class="div_costumer">
+				<label class="desc" id="title1" for="Field1">Client</label>
+				<select id="category" class="field select medium" required> 
+					<option value="First Choice">Filipe</option>
+					<option value="Second Choice">Luis</option>
+					<option value="Third Choice">André</option>
+				</select>
+
+		  </div>
+			
+		
+		  <div class="div_category">
+				<label class="desc" id="title1" for="Field1">Client</label>
+				<select id="category" class="field select medium" required> 
+					<option value="First Choice">First Choice</option>
+					<option value="Second Choice">Second Choice</option>
+					<option value="Third Choice">Third Choice</option>
+				</select>
 			</div>
-		  </div>
-			
-		  <div>
-			<label class="desc" id="title3" for="Field3">
-			  Email
-			</label>
-			<div>
-			  <input id="Field3" name="Field3" type="email" spellcheck="false" value="" maxlength="255" tabindex="3"> 
-		   </div>
-		  </div>
-			
-		  <div>
-			<label class="desc" id="title4" for="Field4">
-			  Message
-			</label>
-		  
-			<div>
-			  <textarea id="Field4" name="Field4" spellcheck="true" rows="10" cols="50" tabindex="4"></textarea>
+
+ 	
+			<div class="div_product">
+				<label class="desc" id="title1" for="Field1">Client</label>
+				<select id="product" class="field select medium"  required> 
+					<option value="First Choice">First Choice</option>
+					<option value="Second Choice">Second Choice</option>
+					<option value="Third Choice">Third Choice</option>
+				</select>
 			</div>
+
+			<div class="prod_quantity">
+			<label class="desc" id="title1" for="Field1">Quantity:</label>
+			  <input type="number" class="quantity field text fn" value="" size="8" tabindex="1" min="1" required>
 		  </div>
-			
-		  <div>
-			<fieldset>
-			
-			  <legend id="title5" class="desc">
-				Select a Choice
-			  </legend>
-			  
-			  <div>
-				<input id="radioDefault_5" name="Field5" type="hidden" value="">
-				<div>
-					<input id="Field5_0" name="Field5" type="radio" value="First Choice" tabindex="5" checked="checked">
-					<label class="choice" for="Field5_0">First Choice</label>
-				</div>
-				<div>
-					<input id="Field5_1" name="Field5" type="radio" value="Second Choice" tabindex="6">
-					<label class="choice" for="Field5_1">Second Choice</label>
-				</div>
-				<div>
-					<input id="Field5_2" name="Field5" type="radio" value="Third Choice" tabindex="7">
-					<label class="choice" for="Field5_2">Third Choice</label>
-				</div>
-			  </div>
-			</fieldset>
-		  </div>
-		  
-		  <div>
-			<fieldset>
-			  <legend id="title6" class="desc">
-				Check All That Apply
-			  </legend>
-			  <div>
-			  <div>
-				<input id="Field6" name="Field6" type="checkbox" value="First Choice" tabindex="8">
-				<label class="choice" for="Field6">First Choice</label>
-			  </div>
-			  <div>
-				<input id="Field7" name="Field7" type="checkbox" value="Second Choice" tabindex="9">
-				<label class="choice" for="Field7">Second Choice</label>
-			  </div>
-			  <div>
-				<input id="Field8" name="Field8" type="checkbox" value="Third Choice" tabindex="10">
-				<label class="choice" for="Field8">Third Choice</label>
-			  </span>
-			  </div>
-			</fieldset>
-		  </div>
-		  
-		  <div>
-			<label class="desc" id="title106" for="Field106">
-				Select a Choice
-			</label>
-			<div>
-			<select id="Field106" name="Field106" class="field select medium" tabindex="11"> 
-			  <option value="First Choice">First Choice</option>
-			  <option value="Second Choice">Second Choice</option>
-			  <option value="Third Choice">Third Choice</option>
-			</select>
-			</div>
-		  </div>
-		  
-		  <div>
-				<div>
+
+				<div id="submit">
 				<input id="saveForm" name="saveForm" type="submit" value="Submit">
 			</div>
-			</div>
+
 		  
 		</form>
 	</body>
